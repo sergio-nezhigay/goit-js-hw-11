aa75177212a11929c0d19fc4a832b990dc07cb25
Sergio Nezhigay <abc1971h@gmail.com>
2023-03-10 21:40:26 +0200

    works but without infinite scroll

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = '34178122-6f2f44cafc8883bb29b8df8c2';
 const BASE_URL = 'https://pixabay.com/api';
 
@@ -12,20 +14,20 @@ class FetchAPI {
     });
   }
 
-  fetchImages({ searchQuery, page, per_page }) {
-    this.params.set('q', searchQuery);
-    this.params.set('page', page);
-    this.params.set('per_page', per_page);
+  async fetchImages({ searchQuery, page, per_page }) {
+    try {
+      this.params.set('q', searchQuery);
+      this.params.set('page', page);
+      this.params.set('per_page', per_page);
+      const url = new URL(BASE_URL);
+      url.search = this.params;
 
-    const url = new URL(BASE_URL);
-    url.search = this.params;
+      let res = await axios.get(url);
 
-    return fetch(url).then(response => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    });
+      return res.data;
+    } catch (error) {
+      throw new Error(`API Error in fetch: ${error.message}`);
+    }
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,50 +1,56 @@
-import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './css/styles.css';
+
+import SimpleLightbox from 'simplelightbox';
 import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 
 import { FetchAPI } from './fetchAPI';
-
 import { imagesMarkup } from './templates';
 
 const element = {
   input: document.querySelector('.search-form'),
   loadmoreButton: document.querySelector('.load-more'),
   gallery: document.querySelector('.gallery'),
+  deleteSearchButton: document.querySelector('.search-form__button--delete'),
 };
 
 const per_page = 40;
 let page = 1;
 let gallery = null;
+let searchQuery;
 const api = new FetchAPI();
-let searchQuery = 'black';
 
-fetchAndShow(searchQuery);
-
-function fetchAndShow() {
-  element.loadmoreButton.classList.add('hidden');
-
-  api
-    .fetchImages({ searchQuery, page, per_page })
-    .then(({ hits, totalHits }) => {
-      showImages(hits);
-      if (!hits.length) {
-        Notiflix.Notify.warning(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        if (page === 1) {
-          Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-        }
-        if (page++ <= totalHits / per_page) {
-          element.loadmoreButton.classList.remove('hidden');
-        } else {
-          Notiflix.Notify.warning(
-            "We're sorry, but you've reached the end of search results."
-          );
-        }
-      }
+async function fetchAndShow() {
+  try {
+    const { hits = [], totalHits = 0 } = await api.fetchImages({
+      searchQuery,
+      page,
+      per_page,
     });
+
+    if (hits.length) {
+      showImages(hits);
+      if (page === 1) {
+        Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+      }
+
+      if (page++ <= totalHits / per_page) {
+        element.loadmoreButton.classList.remove('hidden');
+      } else {
+        Notiflix.Notify.warning(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    } else {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+  } catch (e) {
+    console.log('Error in app: ', e);
+  }
 }
 
 function showImages(images) {
@@ -67,19 +73,42 @@ function showImages(images) {
   }
 }
 
-element.loadmoreButton.addEventListener('click', fetchAndShow);
+element.loadmoreButton.addEventListener('click', () => fetchAndShow());
 
 element.input.addEventListener('submit', onSubmit);
 function onSubmit(e) {
   e.preventDefault();
-  searchQuery = e.target.elements.searchQuery.value.trim();
+  ({
+    searchQuery: { value: searchQuery },
+  } = e.target.elements);
   if (searchQuery) {
-    page = 1;
-    if (gallery) {
-      gallery.destroy();
-      gallery = null;
-    }
-    element.gallery.innerHTML = '';
+    clearGallery();
     fetchAndShow();
   }
+}
+
+function clearGallery() {
+  page = 1;
+  if (gallery) {
+    gallery.destroy();
+    gallery = null;
+    element.gallery.innerHTML = '';
+  }
+  element.loadmoreButton.classList.add('hidden');
+}
+
+element.deleteSearchButton.addEventListener('click', onDeleteSearchButton);
+
+function onDeleteSearchButton() {
+  element.input.reset();
+  clearGallery();
+  element.deleteSearchButton.classList.add('hidden');
+}
+
+element.input.addEventListener('input', debounce(onInput, 500));
+
+function onInput(e) {
+  if (e.target.value.length > 0)
+    element.deleteSearchButton.classList.remove('hidden');
+  else element.deleteSearchButton.classList.add('hidden');
 }
